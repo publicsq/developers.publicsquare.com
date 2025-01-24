c0077bf60e800776f469f818aeb54141853ab760
rlfrahm <frahmryan@gmail.com>
2025-01-24 14:28:46 -0600

    feat: new doc, style fixes, wording fixes, link fixes

--- sidebars.ts
+++ sidebars.ts
@@ -21,7 +21,7 @@ import financialApiSidebar from "./docs/api/financial/sidebar";
      {
        type: "category",
        label: "Merchants",
-       items: ["guides/merchants/quick-start", "guides/merchants/collect-cards", "guides/merchants/process-payments", "guides/merchants/issue-refunds", "guides/merchants/view-transactions"],
+       items: ["guides/merchants/quick-start", "guides/merchants/collect-cards", "guides/merchants/process-payments", "guides/merchants/collect-bank-accounts", "guides/merchants/process-ach-payments", "guides/merchants/issue-refunds", "guides/merchants/view-transactions"],
      },
      {
        type: "category",
